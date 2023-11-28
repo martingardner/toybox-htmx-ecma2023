9f5148937694666272767cd949aef3766093b605
Martin Gardner II <martingardnerii@gmail.com>
2023-11-28 10:42:20 -0600

    try out some of the ecma2023 new array methods

--- ecma2023/ecma2023.js
+++ ecma2023/ecma2023.js
@@ -1,1 +1,38 @@
-console.log('hello');
+const users = [
+    {id: 1, name: 'user1', age: 19},
+    {id: 2, name: 'user2', age: 23},
+    {id: 3, name: 'user3', age: 44},
+    {id: 4, name: 'user4', age: 21},
+    {id: 5, name: 'user5', age: 42}
+];
+
+//findLast();
+const findLastUser = users.findLast( u => u.age < 30);
+console.log('findLastUser', findLastUser);
+
+//findLastIndex();
+const findLastIndexUser = users.findLastIndex( u => u.age < 30);
+console.log('findLastIndexUser', findLastIndexUser)
+
+//toReversed(); non-destructive array change //toSorted() does same thing same as //toSpliced
+const numArr = [1, 2, 3, 4, 5];
+const numArr2 = [1, 2, 3, 4, 5];
+
+const reverseNum = numArr.reverse();
+console.log('numArr', numArr);
+console.log('reverseNum', reverseNum);
+
+const reverseNumTo = numArr2.toReversed();
+console.log('numArr', numArr2);
+console.log('reverseNumTo', reverseNumTo);
+
+// Array with() for updating an instance of an array non-destructive
+const usernames = ['user1', 'user2', 'user3'];
+const usernames2 = [...usernames];
+// original way
+usernames[1] = 'newUser';
+console.log('usernames', usernames);
+// .with
+const updatedUsernames2 = usernames2.with(1, 'newUser');
+console.log('updatedUsernames2', updatedUsernames2);
+console.log('usernames2', usernames2);
