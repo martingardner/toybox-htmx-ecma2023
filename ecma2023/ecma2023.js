e87a3b3318901fb6b0cbdbaec35aa90015fdb319
Martin Gardner II <martingardnerii@gmail.com>
2023-12-05 11:54:39 -0600

    looking at object groupBy

--- ecma2023/ecma2023.js
+++ ecma2023/ecma2023.js
@@ -36,3 +36,18 @@ console.log('usernames', usernames);
 const updatedUsernames2 = usernames2.with(1, 'newUser');
 console.log('updatedUsernames2', updatedUsernames2);
 console.log('usernames2', usernames2);
+
+
+//groupby
+const arrObj = [
+    {type: 'Fish', id: 1, name: 'Salmon'},
+    {type: 'Fish', id: 2, name: 'Northern'},
+    {type: 'Animal', id: 3, name: 'Pig'},
+    {type: 'Fish', id: 4, name: 'Walleye'},
+    {type: 'Animal', id: 5, name: 'Cow'},
+];
+
+const typeGroupBy = Object.groupBy(arrObj, ({type}) => type);
+
+console.log('typeGroupBy', typeGroupBy);
+console.log('arrObj', arrObj);
