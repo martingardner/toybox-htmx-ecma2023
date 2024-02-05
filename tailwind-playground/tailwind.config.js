08d6ab41b19f2931725a8cc6dede5c5776d8eb51
Martin Gardner II <martingardnerii@gmail.com>
2024-02-05 12:39:59 -0600

    initial image with gradient border around it

--- tailwind-playground/tailwind.config.js
+++ tailwind-playground/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html, js}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '200': '200px'
+      },
+      height: {
+        '200': '200px'
+      },
+      backgroundImage: {
+        'radial-gradient': 'radial-gradient(circle, rgba(191,236,238,0.5) 0%, rgba(0,110,122,0.5) 50%, rgba(191,236,238,1) 100%)'
+      }
+    },
   },
   plugins: [],
 }
