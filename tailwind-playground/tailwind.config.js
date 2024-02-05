cbfae50795d9c7ad47961f26849b9fdcd820c926
Martin Gardner II <martingardnerii@gmail.com>
2024-02-05 14:34:19 -0600

    circular gradient background image, transition hover on it

--- tailwind-playground/tailwind.config.js
+++ tailwind-playground/tailwind.config.js
@@ -4,14 +4,19 @@ module.exports = {
   theme: {
     extend: {
       width: {
-        '200': '200px'
+        '200': '200px',
+        '384': '384px'
       },
       height: {
-        '200': '200px'
+        '100': '100px',
+        '200': '200px',
+        '234': '234px'
       },
       backgroundImage: {
         'radial-gradient': 'radial-gradient(circle, rgba(191,236,238,0.5) 0%, rgba(0,110,122,0.5) 50%, rgba(191,236,238,1) 100%)'
-      }
+      },
+
+
     },
   },
   plugins: [],
