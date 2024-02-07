c24dc01cc48b644d9a6911664c5fcd13876200b8
Martin Gardner II <martingardnerii@gmail.com>
2024-02-06 20:23:55 -0600

    border based polygon shape

--- tailwind-playground/tailwind.config.js
+++ tailwind-playground/tailwind.config.js
@@ -15,8 +15,6 @@ module.exports = {
       backgroundImage: {
         'radial-gradient': 'radial-gradient(circle, rgba(191,236,238,0.5) 0%, rgba(0,110,122,0.5) 50%, rgba(191,236,238,1) 100%)'
       },
-
-
     },
   },
   plugins: [],
